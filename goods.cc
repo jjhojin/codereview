28fc4c22a8684bea2406a2bd4f472fad8db927f0
jjhojin <43946833+jjhojin@users.noreply.github.com>
2019-06-12 15:20:26 +0900

    add constructor and destructor

--- goods.cc
+++ goods.cc
@@ -1,5 +1,17 @@
 #include "./goods.h"
 
+Goods::Goods() : goods_index_(0), goods_price_ (0)
+{
+}
+
+Goods::Goods(int index, int price) : goods_index_(index), goods_price_(price)
+{
+}
+
+Goods::~Goods()
+{
+}
+
 // Set member variable of goods with given index and price values
 void Goods::SetGoodsInfo(int index, int price) {
   goods_index_ = index;
